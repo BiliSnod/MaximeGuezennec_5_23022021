531c18f5aec4c64ffef256ea85c726ed769ab695
BiliSnod <mxmgznnc@gmail.com>
2021-07-14 01:07:30 +0200

    ADD object creation after a click on 'add to cart' button

--- assets/js/item.js
+++ assets/js/item.js
@@ -37,6 +37,8 @@ fetch(`http://localhost:3000/api/teddies/${itemId}`)
 
         /* --- Item color --- */
         const teddyColorSelect = document.createElement("select");  // defining a <select> element to choose between item colors
+        teddyColorSelect.setAttribute("id", "select-color");  // setting and ID for the <input>
+        teddyColorSelect.setAttribute("name", "select-color");  // setting and ID for the <input>
         teddyItem.appendChild(teddyColorSelect);  // adding the image inside the <article> element
 
         const colors = teddy.colors; 
@@ -52,43 +54,59 @@ fetch(`http://localhost:3000/api/teddies/${itemId}`)
 
 
         /* --- Item quantity --- */
-        const teddyQuantityNumber = document.createElement("input");  // defining an <input> element for quantity
-        teddyQuantityNumber.setAttribute("id", "quantity");  // setting and ID for the <input>
-        teddyQuantityNumber.setAttribute("name", "quantity");  // setting and ID for the <input>
-        teddyQuantityNumber.setAttribute("size", 1);  // limiting the width of the <input>
-        teddyQuantityNumber.setAttribute("type", "number");  // limiting the length to one character
-        teddyQuantityNumber.setAttribute("value", 1);  // filling the "value" attribute to 1
-        teddyQuantityNumber.setAttribute("min", 1);  // limiting the "value" from 1...
-        teddyQuantityNumber.setAttribute("max", 9);  // ...to 9
-        teddyItem.appendChild(teddyQuantityNumber);  // adding the <input> inside the <article> element
-        /* ------ */
+        const teddyQuantitySelect = document.createElement("select");  // defining a <select> element for quantity
+        teddyQuantitySelect.setAttribute("id", "select-quantity");  // setting and ID for the <input>
+        teddyQuantitySelect.setAttribute("name", "select-quantity");  // setting and ID for the <input>
+        teddyItem.appendChild(teddyQuantitySelect);  // adding the <input> inside the <article> element
 
+        const quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for (const quantity of quantities) {
+            const teddyQuantityNumber = document.createElement("option");  // defining an <option> element for quantity
+            teddyQuantityNumber.setAttribute("value", quantity);  // filling the value attribute with the quantity
+            teddyQuantityNumber.textContent = quantity;  // filling the <option> element with the quantity
+            teddyQuantitySelect.appendChild(teddyQuantityNumber);  // adding the <option> inside the <select> element
+        }
+        /* ------ */
 
             
         /* --- Item price --- */
         const teddyPrice = document.createElement("p");  // defining a <p> element for each item
-        teddyPrice.setAttribute("class", "price");  // setting and ID for the <input>
         teddyPrice.textContent = (teddy.price / 100) + ",00€";  // filling the paragraph with the price displayed in euros
         teddyItem.appendChild(teddyPrice);  // adding the price inside the <article> element
-        
-        const priceQuantity = document.getElementById("quantity");  // modifying the displayed price when quantity change
-        priceQuantity.addEventListener('change', (event) => {  // placing EventListener on the "quantity" input
-            const totalPriceQuantity = (teddy.price * event.target.value) / 100;  // calculating the price with a new quantity
-            const displayTotalPriceQuantity = document.querySelector('.price');  // 
-            displayTotalPriceQuantity.textContent = totalPriceQuantity + ",00€";
-        });
         /* ------ */
         
+
+
 
         /* --- Add to cart --- */
         document.getElementById("item");  // targeting the "item" <section>
         
-        const add = document.createElement("div");  // defining an <div> element
-        item.appendChild(add);  // adding the <div> inside the "item" <section>
+        const toCart = document.createElement("div");  // defining an <div> element
+        item.appendChild(toCart);  // adding the <div> inside the "item" <section>
 
-        const addToCart = document.createElement("button");  // defining a <button> element
-        addToCart.textContent = "Ajouter au panier";
-        add.appendChild(addToCart);
+        const buttonToCart = document.createElement("button");  // defining a <button> element
+        buttonToCart.setAttribute("id", "btn__cart");  // defining an ID for the <button>
+        buttonToCart.setAttribute("name", "add-to-cart");  // defining a name for the <button>
+        buttonToCart.setAttribute("type", "submit");  // defining the "submit" type for the <button>
+        buttonToCart.textContent = "Ajouter au panier";  // filling the <button> with text
+
+        toCart.appendChild(buttonToCart);  // adding the <button> to the <div> tag
+
+
+        const sendToCart = document.querySelector("#btn__cart");  // targeting the <button> sending the informations to cart 
+        sendToCart.addEventListener("click", (event) => {  // configuring the events when the button is clicked
+            event.preventDefault();  // preventing default behavior of the <button>
+            
+            const selectQuantity = document.querySelector("#select-quantity");  // targeting the <select> element for quantity
+            const selectQuantityValue = selectQuantity.value;  // getting the selector's value
+
+            const sendingInformations = {  // defining an object with the informations to send to cart
+                sendItemId: teddy._id,  // sending the item ID
+                sendQuantity: selectQuantityValue  // sending the selected quantity
+            };
+            console.log(sendingInformations)
+        });
         /* ------ */
     })
     .catch(error => document.getElementById("item").innerHTML = "<p>Désolé, aucun article n'est disponible.</p>");  // message displayed when a problem occur
+
